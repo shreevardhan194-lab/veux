7c50ca0fbb70419b12420b3095c91fdfac07e3bf
shreevardhan194-lab <shreevardhan194@gmail.com>
2026-04-24 14:34:11 -0700

    Add shopData.js with brands and products data

---
+++ src/data/shopData.js
@@ -0,0 +1,21 @@
+export const BRANDS = [
+  { id:'all',   icon:'✦',  name:'All'   },
+  { id:'zara',  icon:'Z',  name:'Zara'  },
+  { id:'asos',  icon:'AM', name:'ASOS'  },
+  { id:'urban', icon:'UO', name:'Urban' },
+  { id:'mango', icon:'MQ', name:'Mango' },
+  { id:'cos',   icon:'CS', name:'COS'   },
+  { id:'skims', icon:'SK', name:'Skims' },
+  { id:'hm',    icon:'HM', name:'H&M'   },
+];
+
+export const PRODUCTS = [
+  { id:1, emoji:'👗', brand:'Zara',       brandId:'zara',  name:'Satin Bias-Cut Midi Dress',     price:'$39', originalPrice:'$59',  savePct:'34%' },
+  { id:2, emoji:'🧥', brand:'Mango',      brandId:'mango', name:'Longline Wool-Blend Coat Beige', price:'$89', originalPrice:'$129', savePct:'31%' },
+  { id:3, emoji:'👟', brand:'ASOS',       brandId:'asos',  name:'Platform Chelsea Boot Black',   price:'$55', originalPrice:'$85',  savePct:'35%' },
+  { id:4, emoji:'👜', brand:'Urban Out.', brandId:'urban', name:'Faux Leather Shoulder Bag Tan', price:'$38', originalPrice:'$54',  savePct:'30%' },
+  { id:5, emoji:'🩱', brand:'Skims',      brandId:'skims', name:'Cotton Jersey Bodysuit Sand',   price:'$30', originalPrice:'$42',  savePct:'29%' },
+  { id:6, emoji:'🧣', brand:'COS',        brandId:'cos',   name:'Merino Rib-Knit Scarf Oat',     price:'$39', originalPrice:'$55',  savePct:'29%' },
+  { id:7, emoji:'🎀', brand:'Zara',       brandId:'zara',  name:'Satin Bow-Tie Blouse Ivory',    price:'$28', originalPrice:'$45',  savePct:'38%' },
+  { id:8, emoji:'🩴', brand:'H&M',        brandId:'hm',    name:'Strappy Leather Flat Sandal',   price:'$22', originalPrice:'$35',  savePct:'37%' },
+];
